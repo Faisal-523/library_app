dffcbb620e0b1c43b125cc460fe6e57559c5359b
Faisal-523 <faisalbukhari523@gmail.com>
2020-12-04 20:58:58 +0530

    improved the logic for input and button tags

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 //Class to create book
 const book = class book{
     
@@ -26,46 +24,27 @@ const book = class book{
     }
 }
 
-//domCache function handles DOM manipulation activities
-const domCache = (()=>{
-    this.mainHeader = document.querySelector(".main-header").getElementsByTagName('*');
-    this.myForm = Array.from(Array.from(mainHeader).filter((items)=>items.getAttribute('class') === 'form-container'))[0];
-    domList = [{name:'mainheader', container:this.mainHeader},{name:'myform',container:this.myForm}];
-
-
-    function find(obj){
-        let elem;
-        domList.forEach(item=>{
-            if(item.name == obj.name){
-                elem = Array.from(domList[0].container).filter((element) => element.getAttribute(obj.attribute) == obj.match);
-                console.log(elem);
-            }
-        });
-        return elem;
-    }
-    return {find};
-})();
-
 //This module contains the methods for modifying the list of books in the library
 const myLibrary = (()=>{
     let bookList = [];
-    this.addBtn = domCache.find({name:'mainheader', attribute:'class',match:'add-btn'})[0];
-    this.formContainer = domCache.find({name:'myform', attribute:'id',match:'myForm'})[0];
-    this.submitBtn = domCache.find({name:'myform', attribute:'id',match:'submit'})[0];
-    this.cancelBtn = domCache.find({name:'myform', attribute:'id',match:'cancel'})[0];
-
+    this.mainHeader = document.querySelector(".main-header").getElementsByTagName('*');
+    this.input = Array.from(mainHeader).filter((item=>item.tagName == 'INPUT'));
+    this.buttons = Array.from(mainHeader).filter((item=>item.tagName == 'BUTTON'));
 
     function addBook(obj){
 
         let nBook = new book();
+        console.log(obj);
         for(const prop in nBook){
             if(prop == 'status'){
-                let statusArray = domCache.find({name:'myform', attribute:'name',match:'readStatus'});
-                nBook[prop] = obj[prop] || statusArray.filter(item => item.checked==true)[0].value;
+                //console.log(obj.status || input.filter(item=>item.name=='readStatus' && item.checked == true)[0].value);
+
+                nBook[prop] = obj ? obj[prop] : input.filter(item=>item.name=='readStatus' && item.checked == true)[0].value; //if correct obj is passed then take that as input. Otherwise, use html element
+                //nBook[prop] = obj[prop] || statusArray.filter(item => item.checked==true)[0].value;
             }
             else{
-                console.log(prop);
-                nBook[prop] = obj[prop] || domCache.find({name:'myform', attribute:'id',match:prop})[0].value;
+                nBook[prop] = obj ? obj[prop] : input.filter(item=>item.id==prop)[0].value; //if correct obj is passed then take that as input. Otherwise, use html element
+                //console.log(input.filter(item=>item.id==prop)[0].value)
             }
         };
         bookList.push(nBook);
@@ -89,33 +68,18 @@ const myLibrary = (()=>{
         displayControl.render();
     }
 
-    //let boundAddBook = addBook.bind(this); //Bind the function so that 'this' can be used properly
 
     function initBtns(){
-        //console.table(mainHeader);
-        //Array.from(mainHeader).forEach(item=>console.log(item.getAttribute('id')));
-        //console.log(addBtn);
-        //console.log(submitBtn);
         addBook({title:'Title', author:'Author',pages:'Pages',status:'Status'});
 
-       addBtn.addEventListener('click',function(e){
-           console.log('add btn clicked');
-        formContainer.style.display = 'block';   
-       });
-
-       cancelBtn.addEventListener('click',function(e){
-        console.log('cancel btn clicked');
-        formContainer.style.display = 'none';   
-       });
-
-       submitBtn.addEventListener('click',function(e){
-        console.log('submit btn clicked');
-        formContainer.style.display = 'none';  
-        addBook(); 
-       });
-       //console.log(Array.from(myForm).forEach(item =>console.log(item.id)));
-       //console.log
-        
+        buttons.forEach(element=>{
+            element.addEventListener('click',function(e){
+                console.log(`${element.id} btn clicked`);
+                    Array.from(mainHeader).filter(item=>item.id == 'myForm')[0].style.display = (element.id == 'add'?'block':'none');
+                    if(this.id == 'submit')
+                    addBook();
+            });
+        });
     }
 
     return{initBtns, removeBook, updateBook}
